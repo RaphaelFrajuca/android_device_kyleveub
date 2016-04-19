ea7cbff6478a037925023063b200162a9ce7da0b
Raphael <olocogameplays552@gmail.com>
2016-04-19 17:15:46 -0300

    init kyleveub cm-10.2

---
+++ README.md
@@ -0,0 +1,5 @@
+android_device_kyleveub
+======================
+
+Device tree for Galaxy Trend Lite GT-S7392L (2 SIM)
+Tree forked to work in kyleveub variant
---
+++ cm.mk
@@ -0,0 +1,16 @@
+# Release name
+PRODUCT_RELEASE_NAME := kyleveub
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/kyleveub/device_kyleveub.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kyleveub
+PRODUCT_NAME := cm_kyleveub
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := GT-S7392L
+PRODUCT_CHARACTERISTICS := phone
